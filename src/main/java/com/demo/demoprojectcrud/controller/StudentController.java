1d889e4e910904117a29145876fdc4ec3dc1df66
Jamadagyajit-Swain <2041019336.jamadagyajitswain@gmail.com>
2025-09-26 15:02:04 +0530

    Applied PutAPI and changed student details of few students

--- src/main/java/com/demo/demoprojectcrud/controller/StudentController.java
+++ src/main/java/com/demo/demoprojectcrud/controller/StudentController.java
@@ -7,16 +7,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+
 public class StudentController {
 
     private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
+
         this.studentService = studentService;
     }
 
     @PostMapping("/student")
     public ResponseEntity<StudentResponseDTO> createStudent(@RequestBody StudentRequestDTO studentRequestDTO) {
         return ResponseEntity.ok(studentService.createStudent(studentRequestDTO));
+    }
+
+    @PutMapping("/{id}")
+
+    public ResponseEntity<StudentResponseDTO>updateStudent(@PathVariable Long id, @RequestBody StudentRequestDTO studentRequestDTO){
+
+        StudentResponseDTO response = studentService.updateStudent(id, studentRequestDTO);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/demo/demoprojectcrud/service/StudentService.java
+++ src/main/java/com/demo/demoprojectcrud/service/StudentService.java
@@ -15,17 +15,36 @@ public class StudentService {
     private final StudentRepository studentRepository;
     private final StudentMapper studentMapper;
     private final ModelMapper modelMapper;
+//    private final StudentRequestDTO studentRequestDTO;
+
 
     public StudentService(StudentRepository studentRepository, StudentMapper studentMapper, ModelMapper modelMapper) {
         this.studentRepository = studentRepository;
         this.studentMapper = studentMapper;
         this.modelMapper = modelMapper;
+//        this.studentRequestDTO = studentRequestDTO;
     }
 
     public StudentResponseDTO createStudent(StudentRequestDTO studentRequestDTO) {
         Student student = new Student();
         BeanUtils.copyProperties(studentRequestDTO, student);
         Student savedStudent = studentRepository.save(student);
-        return studentMapper.toDTO(savedStudent, modelMapper);
+        return studentMapper.toDTO(savedStudent);
+    }
+
+    public StudentResponseDTO updateStudent(Long id, StudentRequestDTO studentRequestDTO){
+        Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student Not Found"));
+        BeanUtils.copyProperties(studentRequestDTO, student);
+
+        student.setStudentName(studentRequestDTO.getStudentName());
+        student.setStudentEmail(studentRequestDTO.getStudentEmail());
+        student.setStudentPhone(studentRequestDTO.getStudentPhone());
+        student.setStudentAddress(studentRequestDTO.getStudentAddress());
+        student.setStudentCourse(studentRequestDTO.getStudentCourse());
+        student.setStudentRollNumber(studentRequestDTO.getStudentRollNumber());
+        student.setStudentAge(studentRequestDTO.getStudentAge());
+
+        Student updatedStudent = studentRepository.save(student);
+        return studentMapper.toDTO(updatedStudent);
     }
 }
