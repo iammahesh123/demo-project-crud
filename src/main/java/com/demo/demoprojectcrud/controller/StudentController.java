2d84cb50d189fb5861aa7b81c47d8fc0c3a0c90c
Jamadagyajit-Swain <2041019336.jamadagyajitswain@gmail.com>
2025-09-29 14:51:48 +0530

    Applied deleteAPI

--- src/main/java/com/demo/demoprojectcrud/controller/StudentController.java
+++ src/main/java/com/demo/demoprojectcrud/controller/StudentController.java
@@ -13,7 +13,6 @@ public class StudentController {
     private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
-
         this.studentService = studentService;
     }
 
@@ -21,16 +20,20 @@ public class StudentController {
     public ResponseEntity<StudentResponseDTO> createStudent(@RequestBody StudentRequestDTO studentRequestDTO) {
         return ResponseEntity.ok(studentService.createStudent(studentRequestDTO));
     }
-//
-@PutMapping("/{id}")
-public ResponseEntity<StudentResponseDTO> updateStudent(@PathVariable Long id, @RequestBody StudentRequestDTO studentRequestDTO) {
 
-    StudentResponseDTO response = studentService.updateStudent(id, studentRequestDTO);
-    return ResponseEntity.ok(response);
-  }
-  @GetMapping("/{id}")
-public ResponseEntity<StudentResponseDTO> getById (@PathVariable Long id) {
-        StudentResponseDTO response = studentService.getById(id);
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> updateStudent(@PathVariable Long id, @RequestBody StudentRequestDTO studentRequestDTO) {
+        StudentResponseDTO response = studentService.updateStudent(id, studentRequestDTO);
         return ResponseEntity.ok(response);
- }
+  }
+   @GetMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> getById (@PathVariable Long id) {
+       StudentResponseDTO response = studentService.getById(id);
+       return ResponseEntity.ok(response);
+   }
+   @DeleteMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> deleteById (@PathVariable Long id) {
+        StudentResponseDTO response = studentService.deleteById(id);
+        return ResponseEntity.ok(response);
+   }
 }
